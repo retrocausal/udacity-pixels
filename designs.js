e13955453f14e8c2b1fb162d2d8965d33b6113ac
Nitin Prabhakar <pmitwithnitin@gmail.com>
2017-12-19 19:57:17 +0530

    Bug Fixes  - The default color of the color picker needs to be the current background color of the canvas cell clicked  - The Canvas need a MAX and a MIN Dimension set  - The User needs to be able to not only paint a single cell at a time, BUT, ALSO,     - To be able to paint multiple cells at a time

--- designs.js
+++ designs.js
@@ -72,10 +72,13 @@ $(() => {
               "width": `${cssW}px`,
               "height": `${cssH}px`,
               "max-width": `${cssW}px`,
-              "max-height": `${cssH}px`
+              "max-height": `${cssH}px`,
+              //set initial background color
+              "background-color": "#fff"
             })
+            .attr("data-current-bg-color", "#ffffff")
             .hide()
-            .fadeIn(5000));
+            .fadeIn(3000));
       }
       //end length
     }
@@ -85,8 +88,54 @@ $(() => {
     $("#caption")
       .html($('<h2> canvas </h2>')
         .hide()
-        .fadeIn(5000));
-    return activateCanvas();
+        .fadeIn(3000));
+    //unhide the multiple cell painter
+    $(".multiplier")
+      .hide()
+      .fadeIn(2000);
+    //listen for global color pick event
+    $("#multiple-cell-painter")
+      .change((eMCP) => {
+        const form = document.querySelector("#multiple-paint-enabled");
+        if (!form) {
+          const enableMCP = $('<input id="multiple-paint-enabled" name="mcpEnabled" type="checkbox" value="yes" checked>')
+            .css({
+              "min-width": "40px",
+              "min-height": "40px"
+            });
+          enableMCP.change(reactivateCanvas);
+          $(".mcpform")
+            .append(enableMCP);
+        }
+        return activateCanvas(true);
+      });
+
+    return activateCanvas(false);
+  };
+  /*
+   *@name reactivateCanvas
+   *@params[emcpCB] event handler obtained event Object
+   *@description Registered change handler for the toggler between multiple and single cell paint setter
+   */
+  const reactivateCanvas = (emcpCB) => {
+    const checked = $(emcpCB.target)
+      .attr("checked");
+    const val = $(emcpCB.target)
+      .val();
+    if (checked) {
+      $(emcpCB.target)
+        .removeAttr("checked");
+      $(emcpCB.target)
+        .attr("value", "no");
+      $(emcpCB.target)
+        .remove();
+      return activateCanvas(false);
+    }
+    $(emcpCB.target)
+      .attr("checked", "checked");
+    $(emcpCB.target)
+      .attr("value", "yes");
+    return false;
   };
 
   /*
@@ -98,58 +147,100 @@ $(() => {
    *Each canvas cell, activates EXACTLY ONE color-picker, and later removes it from the DOM.
    @events "onclick" = > canvas cell , ["onchange" ,"onmouseover" ,"onmouseleave"] = > input[color]
    */
-  const activateCanvas = () => {
+  const activateCanvas = (multiple) => {
     //listen to click events on each cell
-    return $(".canvas-cell")
-      .on("click", (eCell) => {
-        //create a new input dedicated to a cell, hide by default until required
-        let colorPicker = $('<input type="color"></input>')
-          .hide();
-        let id = $(eCell.target)
-          .attr("id");
-        colorPicker.attr("id", `color-picker-${id}`);
-        //gather page co-ordinates where the click event happened
-        const x = eCell.pageX;
-        const y = eCell.pageY;
-        //push the color picker input to the exact click location
-        colorPicker.css({
-          "position": "absolute",
-          "top": `${y - 20}px`,
-          "left": `${x}px`,
-          "z-index": 100
+    $(".canvas-cell")
+      .off("click");
+    if (multiple) {
+      return $(".canvas-cell")
+        .click((eCell) => {
+          const bgColor = $("#multiple-cell-painter")
+            .val();
+          const id = $(eCell.target)
+            .attr("id");
+          return paintCell(id, bgColor);
         });
-        //add the color picker,hide if not already hidden, and fade it into view
-        $("BODY")
-          .append(colorPicker);
-        colorPicker.hide()
-          .fadeIn(2000)
-          // if user moves the mouse away, fadeout in 3
-          .on("mouseleave", (e) => {
-            $(e.target)
-              .fadeOut(3000);
-          })
-          //if the user moves the mouse back to hover, fade back in immediately
-          .on("mouseover", (e) => {
-            $(e.target)
-              .stop()
-              .fadeIn();
-          })
-          //If the user clicks on the input, do nothing
-          .on("click", (e) => {
-            let cpID = $(e.target)
-              .attr("id");
-          })
-          //If the user selects a color, set the background color appropriately
-          .on("change", (e) => {
-            let cpID = $(e.target)
-              .attr("id");
-            //the cell on which the event happened, is now colored by it's own colorPicker
-            $(`#${id}`)
-              .css("background-color", `${$(e.target).val()}`);
-            //job done, remove the dynamic color picker from the DOM
-            $(e.target)
-              .remove();
-          });
+    } else {
+      return $(".canvas-cell")
+        .on("click", initCanvas);
+    }
+  };
+  /*
+   *@name paintCell
+   *@params[id] ID of the canvas cell to be painted
+   *@params[bgColor] color of the canvas cell chosen
+   *@description paints a cell's background to the chosen color
+   */
+  const paintCell = (id, bgColor) => {
+    $(`#${id}`)
+      .attr("data-current-bg-color", bgColor);
+    return $(`#${id}`)
+      .css("background-color", bgColor);
+  };
+  /*
+   *@name initCanvas
+   *@params[eCell] target html elements
+   *@description attach an event handler to handle "click" events
+   */
+  const initCanvas = (eCell) => {
+    //create a new input dedicated to a cell, hide by default until required
+    let colorPicker = $('<input type="color"></input>')
+      .hide();
+    const id = $(eCell.target)
+      .attr("id");
+    colorPicker.attr("id", `color-picker-${id}`);
+    const currentBGColor = $(eCell.target)
+      .attr("data-current-bg-color");
+    //Set the default value to the current background color
+    colorPicker.attr("value", currentBGColor);
+    //gather page co-ordinates where the click event happened
+    const x = eCell.pageX;
+    const y = eCell.pageY;
+    //push the color picker input to the exact click location
+    colorPicker.css({
+      "position": "absolute",
+      "top": `${y}px`,
+      "left": `${x}px`,
+      "z-index": 100,
+      "box-shadow": "0 0 2em 0 #ccc",
+      "width": "6em",
+      "height": "3em"
+    });
+    //add the color picker,hide if not already hidden, and fade it into view
+    $("BODY")
+      .append(colorPicker);
+    return colorPicker
+      .fadeIn()
+      .fadeOut(5000)
+      // if user moves the mouse away, fadeout in 3
+      .on("mouseleave", (e) => {
+        $(e.target)
+          .fadeOut(2000);
+      })
+      //if the user moves the mouse back to hover, fade back in immediately
+      .on("mouseover", (e) => {
+        $(e.target)
+          .stop()
+          .fadeIn();
+      })
+      //If the user clicks on the input, do nothing
+      .on("click", (e) => {
+        $(e.target)
+          .stop()
+          .fadeIn();
+      })
+      .on("input", (e) => {
+        paintCell(id, $(e.target)
+          .val());
+      })
+      //If the user selects a color, set the background color appropriately
+      .on("change", (e) => {
+        //the cell on which the event happened, is now colored by it's own colorPicker
+        paintCell(id, $(e.target)
+          .val());
+        //job done, remove the dynamic color picker from the DOM
+        $(e.target)
+          .remove();
       });
   };
   //Begin listening after DOMContentLoaded, do not submit by default
@@ -158,4 +249,14 @@ $(() => {
       e.preventDefault();
       start();
     });
+  //Set maximum dimensions
+  const wrapper = $(".canvas-wrap")
+    .find("figure");
+  const wrapperWidth = wrapper.width();
+  const maxLength = Math.abs(Math.round(wrapperWidth / 40));
+  const maxDepth = maxLength;
+  $("#input_height")
+    .attr("max", maxDepth);
+  $("#input_width")
+    .attr("max", maxLength);
 });
